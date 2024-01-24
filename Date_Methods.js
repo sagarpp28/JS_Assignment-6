c684bb99725ae91b150c38a37ec58fab34543d92
sagarpp28 <sagarpp28@gmail.com>
2024-01-24 18:13:12 +0530

    first commit

---
+++ Date_Methods.js
@@ -0,0 +1,136 @@
+// New date
+console.log(new Date())
+
+//Date.parse()
+let s = Date.parse('24 jan 2024')
+console.log(s)
+
+// //Date.UTC()
+const utcDate = Date.UTC(2021, 2, 30);
+console.log('UTC Date:', new Date(utcDate));
+
+//getDate()
+ let s=new Date().getDate();
+console.log(s);
+
+//get Month()
+let s=new Date().getMonth();
+console.log(s)
+
+//getFullYear()
+console.log(new Date().getFullYear());
+
+//getDay()
+console.log(new Date().getDay());
+
+//getHours()
+console.log(new Date().getHours());
+
+//getMinutes()
+console.log(new Date().getMinutes());
+
+//get Seconds()
+console.log(new Date().getSeconds());
+
+//getMilliseconds()
+console.log(new Date().getMilliseconds());
+
+//setDate()
+let s=sDate = new Date()
+sDate.setDate(22);
+console.log(s);
+
+//setMonth()
+let s=sDate = new Date()
+sDate.setMonth(8);
+console.log(s);
+
+//setFullYear()
+let s= new Date().setFullYear(2003);
+console.log(new Date(s));
+
+//setHours()
+let s= new Date().setHours(12);
+console.log(new Date(s));
+
+//setMinutes()
+let s= new Date().setMinutes(12);
+console.log(new Date(s));
+
+//setSeconds()
+let s= new Date().setSeconds(12);
+console.log(new Date(s));
+
+//setMilliseconds()
+let s= new Date().setMilliseconds(66);
+console.log(new Date(s));
+
+//toDateString()
+let s= new Date().toDateString();
+console.log(s);
+
+//toLocaleDateString()
+let s= new Date().toLocaleDateString();
+console.log(s);
+
+//toTimeString()
+let s= new Date().toTimeString();
+console.log(s);
+
+//toLocaleTimeString()
+let s= new Date().toLocaleTimeString();
+console.log(s);
+
+//getUTCDate()
+let s = new Date().getUTCDate();
+console.log(s);
+
+//getUTCDay()
+let s = new Date().getUTCDay();
+console.log(s);
+
+//getUTCFullYear()
+let s = new Date().getUTCFullYear();
+console.log(s);
+
+//getUTCHours()
+let s = new Date().getUTCHours();
+console.log(s);
+
+//getUTCMinutes()
+let s = new Date().getUTCMinutes();
+console.log(s);
+
+//getUTCSeconds()
+let s = new Date().getUTCSeconds();
+console.log(s);
+
+//getUTCMilliseconds()
+let s = new Date().getUTCMilliseconds();
+console.log(s);
+
+//setUTCDate()
+let s= new Date().setUTCDate(12);
+console.log(new Date(s));
+
+//setUTCFullYear()
+let s= new Date().setUTCFullYear(2003);
+console.log(new Date(s));
+
+//setUTCHours()
+let s= new Date().setUTCHours(8);
+console.log(new Date(s));
+
+//setUTCMinutes()
+let s= new Date().setUTCMinutes(16);
+console.log(new Date(s));
+
+//setUTCSeconds()
+let s= new Date().setUTCSeconds(29);
+console.log(new Date(s));
+
+//setUTCMilliseconds()
+let s= new Date().setUTCMilliseconds(56);
+console.log(new Date(s));
+
+
---
+++ Math_Methods.js
@@ -0,0 +1,33 @@
+//Math.abs()
+let s= Math.abs(7-5);
+console.log(s);
+
+//Math.ceil()
+console.log (Math.ceil(0.95));
+
+//Math.floor()
+console.log (Math.floor(0.95));
+
+//Math.round()
+console.log(Math.round(0.9));
+
+//Math.max()
+console.log(Math.max(46, 3155, 258));
+
+//Math.min()
+console.log(Math.min(46, 3155, 258));
+
+//Math.pow()
+console.log(Math.pow(7, 3));
+
+//Math.sqrt()
+console.log(Math.sqrt(64));
+
+//Math.trunc()
+console.log(Math.trunc(13.37));
+
+// Math.random()
+console.log(Math.random());
+
+
+
